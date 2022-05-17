6ca97b008b3ce28cf74d52d4edfcfc53d916c150
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-17 13:15:11 +0600

    health sefty database migrate create

--- example-app/app/Models/sefty.php
+++ example-app/app/Models/sefty.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
+class sefty extends Model
 {
     use HasFactory;
-protected $fillable = [
-    'title', 'image', 'description', 'status',
-];
+    protected $fillable = [
+        'title', 'image', 'description', 'status',
+    ];
 }
